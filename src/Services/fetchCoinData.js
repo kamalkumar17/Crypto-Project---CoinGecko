99dfd81ec590134c7c5116fdf7f309242d95837b
kamal kumar <kamalkumar1752000@gmail.com>
2024-12-05 21:03:54 +0530

    Added react query config

--- src/Services/fetchCoinData.js
+++ src/Services/fetchCoinData.js
@@ -1,12 +1,13 @@
-import axios from "axios";
-import axiosInstance from "../Helpers/axiosInstance";
-export async function fetchCoinData(){
-    try{
-        const response = await axiosInstance.get('coins/markets?vs_currency=usd');
+import axiosInstance from "../helpers/axiosInstance";
+
+export async function fetchCoinData(page = 1, currency = "usd") {
+    const perPage = 10;
+    try {
+        const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
         console.log(response);
         return response.data;
-    }
-    catch(error){
-        console.log(error);
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 }
