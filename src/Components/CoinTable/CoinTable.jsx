451eb4257441a6c4a09e95a6b48840ebb7abf1dc
kamal kumar <kamalkumar1752000@gmail.com>
2024-12-07 13:30:23 +0530

    Added coin table ui with cache

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchCoinData } from "../../Services/fetchCoinData";
 import { useQuery } from "react-query";
 
@@ -6,14 +6,11 @@ function CoinTable() {
     const [page, setPage] = useState(1);
     const { data, isLoading, isError, error } = useQuery(['coins', page], () =>
         fetchCoinData(page, 'usd'), {
-        retry: 2,
-        retryDelay: 1000,
+        // retry: 2,
+        // retryDelay: 1000,
         cacheTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60 * 2,
     });
-
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -25,10 +22,66 @@ function CoinTable() {
 
 
     return (
-        <>
-            Coin Table <button onClick={() => setPage(page + 1)}>Click </button>
-            {page}
-        </>
+        <div className="flex flex-col items-center justify-center gap-5 my-5 w-[80vw] mx-auto">
+            <div className="flex items-center justify-center w-full px-2 py-4 font-semibold text-black bg-yellow-500">
+                <div className="basis-[35%]">
+                    Coin
+                </div>
+                <div className="basis-[35%]">
+                    Price
+                </div>
+                <div className="basis-[35%]">
+                    24h Change
+                </div>
+                <div className="basis-[35%]">
+                    Market Cap
+                </div>
+            </div>
+
+            <div className="flex flex-col w-[80vw] mx-auto">
+                {isLoading && <div>Loading....</div>}
+                {data && data.map((coin) => {
+                    return (
+                        <div key={coin.id} className="flex items-center justify-between w-full px-2 py-4 font-semibold text-white bg-transparent">
+                            <div className="flex items-center justify-start gap-3 basis-[35%]">
+                                <div className="h-[5rem] w-[5rem]">
+                                    <img src={coin.image} className="w-full h-full" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <div className="text-3xl">{coin.name}</div>
+                                    <div className="text-xl">{coin.symbol}</div>
+                                </div>
+                            </div>
+
+                            <div className="basis-[35%]">
+                                {coin.current_price}
+                            </div>
+                            <div className="basis-[35%]">
+                                {coin.price_change_24h}
+                            </div>
+                            <div className="basis-[35%]">
+                                {coin.market_cap}
+                            </div>
+                        </div>
+                    )
+                })}
+
+            </div>
+
+            <div className="flex items-center justify-center gap-3">
+                                <button
+                                    disabled={page === 1}
+                                    className="text-2xl text-white btn btn-primary btn-wide"
+                                    onClick={() => setPage(page - 1)}>
+                                    Prev
+                                </button>
+                                <button
+                                    className="text-2xl text-white btn btn-secondary btn-wide"
+                                    onClick={() => setPage(page + 1)}>
+                                    Next
+                                </button>
+                            </div>
+        </div>
     )
 }
 export default CoinTable;
--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -4,5 +4,5 @@ import { COINGECKO_API_URL } from "./Constants";
 const axiosInstance = axios.create({
     baseURL : COINGECKO_API_URL,
 });
-
 export default axiosInstance;
+
